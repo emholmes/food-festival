5fd3e25b2a279315ff6823d4d472df87a727955a
Erin Holmes <e.holmes09@yahoo.com>
2021-09-07 18:35:23 -0500

    remove img width

--- assets/js/script.js
+++ assets/js/script.js
@@ -171,7 +171,7 @@ $(document).ready(function() {
     
     const containerEl = createEl("div", {class: "container"},
       createEl("div", {class: "card mb-3"}, 
-        createEl("img", {class: "card-img-top", style: "width: 5px", src: currentEvent.image || "https://via.placeholder.com/350x150"}),
+        createEl("img", {class: "card-img-top", src: currentEvent.image || "https://via.placeholder.com/350x150"}),
         createEl("div", {class: "card-body"}, 
           createEl("h1", {class: "card-title"}, currentEvent.title || ""),
           createEl("h2", {class: "text-muted"}, currentEvent.subtitle || ""),
